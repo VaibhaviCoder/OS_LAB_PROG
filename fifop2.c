6b9d924d57cd6068fadc137134b60640f0ee9fc6
Vaibhavi Bhushan <97871717+VaibhaviCoder@users.noreply.github.com>
2022-02-20 19:54:26 +0530

    FiFo

---
+++ fifop2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<time.h>
+int main(int argc,char *argv[]){
+        char *myfifo="myfifo";
+  mkfifo(myfifo,0666);
+  int arr[5];  
+        int fd=open("myfifo",O_RDONLY);
+        if(fd==-1){
+  return 1;
+  }
+       if( read(fd,&arr,sizeof(int)*5)==-1){
+       
+                return 3;
+       }
+       printf("Received\n");  
+       close(fd);
+      int sum=0;  
+          int i=0; 
+    for(i=0;i<5;i++){
+      sum+=arr[i];
+    }
+    printf("Sum is: %d\n",sum);
+
+
+  return 0;
+}
